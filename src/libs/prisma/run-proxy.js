5965bcbe2bd35a8a4fffc5cadc9e91391e4c8f68
Marcelo Nogueira <marcelon27@gmail.com>
2024-11-04 18:57:21 -0300

    Config: Running database from gcloud

---
+++ src/libs/prisma/run-proxy.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const { exec } = require('child_process')
+
+const instanceConnectionName = process.env.INSTANCE_CONNECTION_NAME
+
+const command = `./cloud_sql_proxy ${instanceConnectionName}`
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error.message}`)
+    return
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`)
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+  console.info('gcloud running!')
+})

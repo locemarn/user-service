f46a247e3706ef72f34b24fbfe3e9003dd04c64a
Marcelo Nogueira <marcelon27@gmail.com>
2024-11-07 16:57:38 -0300

    Config prisma driverAdapters

--- src/libs/prisma/mock/client.ts
+++ src/libs/prisma/mock/client.ts
@@ -1,15 +1,14 @@
 import { Pool } from 'pg'
-// import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const connectionString =
-  'postgresql://admin:user-service1234@localhost:5432/user-service?schema=public&connection_limit=5&socket_timeout=3'
+const connectionString = `${process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
 
-// const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({
-  // adapter,
+  adapter,
   omit: {
     user: {
       password: true,
